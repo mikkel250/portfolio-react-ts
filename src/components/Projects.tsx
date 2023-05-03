7097c816a991651f69b4fb117b7d21bc13eb5d32
Mikkel Ridley <mikkel.ridley@kulcbd.com>
2023-05-02 19:45:56 -0700

    fix grammar

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -38,7 +38,7 @@ const Projects: React.FC = () => {
         <div className='projects__item'>
           <h2 className='projects__item-title'>Face recognition app</h2>
           <p className='projects__item-desc'>
-            A Full stack web app that allows users to log in/out, and upload photos via links into the user-input field and will fetch, display the photo, recognize faces, and draw boxes around them. Uses React for the UI and front end (this repo) for its ability to update only the required parts of a page, React Router for efficient navigation in React, Express and Node for the backend (see the /face-recognition-api repo) to have REST API’s available for consumption on the frontend, PostgreSQL for the database, and the Clarifai face recognition API to analyze the photos. Technology used: React, Node, Express, REST API’s, PostgreSQL, and the Clarifai API.
+            A Full stack web app that allows users to log in/out, and upload photos via links into the user-input field and will fetch, display the photo, recognize faces, and draw boxes around them. Uses React for the UI and front end (this repo) for its ability to update only the required parts of a page, React Router for efficient navigation in React, Express and Node for the backend (see the /face-recognition-api repo) to have REST APIs available for consumption on the frontend, PostgreSQL for the database, and the Clarifai face recognition API to analyze the photos. Technology used: React, Node, Express, REST APIs, PostgreSQL, and the Clarifai API.
           </p>
         </div>
       </div>
