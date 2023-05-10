4e94f1d81afa4361aed5e99edbe30537a871c085
Mikkel Ridley <mikkel.ridley@kulcbd.com>
2023-05-10 15:56:21 -0700

    update about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () => {
     <div className="about">
       <h1 className="primary">ABOUT</h1>
       <p>Hi, my name is <span className="primary">Mikkel Ridley</span> and I am a <span className="primary">Front End Engineer</span>. Over the past 3+ years, I have honed my skills in Angular, React, Shopify, Jekyll, JavaScript, HTML, and CSS/ SCSS, among many other technologies. Prior to transitioning to a career in software development, I managed all technology-related aspects of a local small business. This experience has provided me with a unique skill set and perspective that I bring to my work as a software developer.</p>
-      <p>Outside of work, I enjoy cycling and traveling. I've been to Mexico, Guatemala, El Salvador, Honduras, Nicaragua, Costa Rica, Egypt, France, Spain, Italy, Switzerland, Czech Republic, Hungary, and Portugal.</p>
+      <p>Outside of work, I enjoy cycling and traveling. I've been to Mexico, Guatemala, El Salvador, Honduras, Nicaragua, Costa Rica, Egypt, France, Spain, Italy, Switzerland, Czech Republic, Hungary, and Portugal. I also enjoy cooking (and, of course, eating), and Mongolian heavy metal.</p>
     </div>
   );
 };
